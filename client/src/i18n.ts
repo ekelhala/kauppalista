0ec58ea5facc51c45060366e948fee6301357a77
Emil Kelhälä <emil.kelhala@protonmail.com>
2026-07-04 17:29:09 +0300

    feat(client): complete i18n initialization with language detection and locale strings

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import fi from './locales/fi/translation.json';
 import en from './locales/en/translation.json';
 
@@ -8,6 +9,7 @@ export type AppLanguage = typeof SUPPORTED_LANGUAGES[number];
 
 void i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     resources: {
       fi: { translation: fi },
@@ -16,6 +18,16 @@ void i18n
     fallbackLng: 'fi',
     supportedLngs: [...SUPPORTED_LANGUAGES],
     interpolation: { escapeValue: false },
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      lookupLocalStorage: 'lang',
+      caches: ['localStorage'],
+    },
   });
+
+document.documentElement.lang = i18n.language;
+i18n.on('languageChanged', (lng) => {
+  document.documentElement.lang = lng;
+});
 
 export default i18n;
